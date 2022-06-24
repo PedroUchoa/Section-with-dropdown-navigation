4a240ff6780c187c44dbc7b6193c12199a40d461
Joao Pedro <joaopedrouchoacampos@gmail.com>
2022-06-24 17:19:18 -0300

    Alterando JS

--- js/script.js
+++ js/script.js
@@ -14,18 +14,18 @@ hoverMenuSecond.forEach(Element =>{
 
 function firstMouseUp (){
     primeiroHover.firstChild.nextSibling.innerHTML = `
-               Features <img src="../images/icon-arrow-up.svg" alt="Icone de seta para cima">
+               Features <img src="images/icon-arrow-up.svg" alt="Icone de seta para cima">
            `
-}
+} 
 function firstMouseDown(){
     primeiroHover.firstChild.nextSibling.innerHTML = `
-            Features <img src="../images/icon-arrow-down.svg" alt="Icone de seta para baixo">
+            Features <img src="images/icon-arrow-down.svg" alt="Icone de seta para baixo">
         `
 }
 
 function secondMouseUp (){
     segundoHover.firstChild.nextSibling.innerHTML = `
-            Company <img src="../images/icon-arrow-up.svg" alt="Icone de seta para cima">
+            Company <img src="images/icon-arrow-up.svg" alt="Icone de seta para cima">
         `
 }
 
